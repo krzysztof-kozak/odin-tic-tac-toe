eb0e9022315273b535bb2de30f0fb98a1934fe75
Kris <krzysztofkoz6@gmail.com>
2021-12-08 20:16:19 +0100

    Add GameBoard module

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log("hello");
+/* 
+GameBoard:
+    - need only one GameBoard, so make it a module.
+    - holds information about the gameboard in form of an array.
+    - can update gamebard.
+*/
+
+const GameBoard = (function () {
+	const _MARKS = {
+		EMPTY: "empty",
+		X: "x",
+		Y: "y",
+	};
+
+	const gameBoard = Array(9).fill({ mark: _MARKS.EMPTY }, 0);
+
+	function _getMark(symbol) {
+		if (symbol === "empty") return { mark: _MARKS.EMPTY };
+		if (symbol === "x") return { mark: _MARKS.X };
+		if (symbol === "y") return { mark: _MARKS.Y };
+	}
+
+	function update(index, mark) {
+		mark = _getMark(mark);
+		gameBoard[index] = mark;
+	}
+
+	return { gameBoard, update };
+})();
